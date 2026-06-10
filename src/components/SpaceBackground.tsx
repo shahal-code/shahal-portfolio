0fafece2552d802209d97f5d46861f656f8b5649
shahal-code <gmrshahl@gmail.com>
2026-06-10 21:23:38 +0530

    improved the u

--- src/components/SpaceBackground.tsx
+++ src/components/SpaceBackground.tsx
@@ -155,6 +155,10 @@ const SpaceBackground = () => {
         };
     }, [isMobile, isDark, isLowPerformance]);
 
+    if (isLowPerformance) {
+        return <div className="fixed inset-0 z-[-1] pointer-events-none bg-black" />;
+    }
+
     return (
         <div
             ref={mountRef}
